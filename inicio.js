94e3b2cf493569b33ea1bfa7a284a27c56a28071
mora <antoniomonotoyaam54@gmail.com>
2025-03-21 12:53:32 -0600

    primer repositorio

---
+++ inicio.js
@@ -0,0 +1,137 @@
+// CREAR UN ARREGLO 
+let arreglo =[1, 2, 3, 4, 5,]
+console.log(arreglo[0])
+arreglo[0]= 2
+
+
+// PUSH
+let array = [1, 2, 3];
+array.push (4)
+console.log(array)
+
+// POP
+
+let array2 = [1, 2, 3];
+array2.pop()
+console.log(array2)
+
+// UNSHIFT 
+miArreglo = ["perro", "gato","pajaro","vaca","elefante"];
+miArreglo.unshift("cuyo")
+console.log(miArreglo)
+
+// SHIFT
+const cities = ["Madrid", "Valencia", "Mexico"];
+const first = cities.shift();
+console.log(cities)
+console.log(first)
+
+// INCLUDE
+
+let miArreglo1 = ["Caballo", "vaca", "burro"];
+ console.log(miArreglo1.includes("Caballo"))
+ console.log(miArreglo1.includes("Persona"))
+
+
+ // INDEX OF
+ console.log(miArreglo1.indexOf("Caballo"))
+
+
+
+// FOR
+ let nuevoArray = ["Caballo", "vaca", "burro"];
+ for (let i = 0; i < nuevoArray; i++ ){
+    console.log (nuevoArray)
+
+ }
+
+ // FILTER
+ let palabras =["Theboss","hola","hamburgesa", "Haitiano", "globo"];
+ let masde5letras = palabras.filter((x)=> x.length >= 5);
+console.log(masde5letras);
+
+// MAP
+
+let number = [1, 4, 9, 16];
+// let raiz =  number.map ((num) => math.sqrt(num));
+//console.log(raiz);
+
+// REDUCE
+
+let inicioen = 1;
+//let operacion= raiz.reduce((acumula, acumulado)=>acumula * acumulado,inicioen);
+//console.log(operacion);
+
+
+// EVERY 
+
+let numP = [6,7,8,9,10]
+
+let Positivos = numP.every(num => num > 0);
+ console.log(Positivos)
+
+ let numN = [1,-2,3,4,5]
+
+ // SOME
+
+
+ let Negativos = numN.some(num => num < 0)
+  console.log(Negativos)
+
+// SHORT
+
+  let nombres = ["Ale", "Fanny","Miguel","Citlalli"];
+  nombres.sort(); 
+  console.log(nombres); 
+
+  // REVERSE
+  let numeros = [1, 2, 3, 4, 5];
+
+  numeros.reverse();
+  console.log(numeros);
+// CONCAT
+  let arreglo1 = [1, 2, 3];
+  let arreglo2 = [4, 5, 6];
+
+  let combinado = arreglo1.concat(arreglo2);
+  console.log(combinado);
+
+  // ------------------------------------------------
+
+
+
+
+   /* LISTA DE ALUMNOS ASISTENCIA : Mañana 9 am unidad 
+   Materia : Programacion Estructurada
+    Recompensa : 5 decimas
+   - Martin
+   - Citlalli
+   - Fanny
+   - Mora 
+   - Frank
+   - Sam
+   - Scar
+   - Alex
+   - Marco
+   - Segundo
+   - Pablo
+   - Alan Cruz
+   - Danna
+   - Cris
+
+   */
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
